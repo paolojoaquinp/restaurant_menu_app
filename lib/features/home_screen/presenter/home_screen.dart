98a04a11d3705a8adb27f93802bae157ac44f3b3
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-12-02 13:26:57 -0400

    feat: stacked ui ListView and transform

--- lib/features/home_screen/presenter/home_screen.dart
+++ lib/features/home_screen/presenter/home_screen.dart
@@ -1,14 +1,28 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:restaurant_menu_app/features/home_screen/presenter/widgets/card_test.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World'),
-     ),
-   );
+      body: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return Transform.translate(
+              offset: Offset(0, -400.0*index),
+              child: Transform(
+                transform: Matrix4.identity()..rotateZ(-pi/(12+index)),
+                child: SizedBox(
+                  height: 500,
+                  width: 300,
+                  child: CardTest(index: index),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
